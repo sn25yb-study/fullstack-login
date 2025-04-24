a15613a7093f7345e94161a608e0ea8a8947dffc
sn25yb <sn25yb.code@gmail.com>
2025-04-24 14:17:25 +0900

    Register Functionality Using fs Module

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,18 +5,20 @@ const fs = require("fs").promises;
 class UserStorage {
 	static #getUserInfo(data, id) {
 		const users = JSON.parse(data);
-				const idx = users.id.indexOf(id);
-				const usersKeys = Object.keys(users); // => [id, pw, name]
-				const userInfo = usersKeys.reduce((newUser, info) => {
-					newUser[info] = users[info][idx];
-					newUser;
-				return newUser;
-			}, {});
-			return userInfo;
+		const idx = users.id.indexOf(id);
+		const usersKeys = Object.keys(users); // => [id, pw, name]
+		const userInfo = usersKeys.reduce((newUser, info) => {
+			newUser[info] = users[info][idx];
+			newUser;
+			return newUser;
+		}, {});
+		return userInfo;
 	}
 
-	static getUsers(...fields) {
-		// const users = this.#users;
+	static #getUsers(data, isAll, fields) {
+		const users = JSON.parse(data);
+		if (isAll)
+			return users;
 		const newUsers = fields.reduce((newUsers, field) => {
 			if (users.hasOwnProperty(field)) {
 				newUsers[field] = users[field];
@@ -24,6 +26,15 @@ class UserStorage {
 			return newUsers;
 		}, {});
 		return newUsers;
+	}
+
+	static getUsers(isAll, ...fields) {
+		return fs
+		.readFile("./src/databases/users.json")
+		.then(data => {
+			return this.#getUsers(data, fields);
+		})
+		.catch(console.error);
 	};
 
 	static getUserInfo(id) {
@@ -36,12 +47,15 @@ class UserStorage {
 		
 	}
 
-	static save(userInfo) {
-		// const users = this.#users;
+	static async save(userInfo) {
+		const users = await this.getUsers(true);
+		if (users.id.includes(userInfo.id)) {
+			throw "이미 존재하는 아이디입니다.";
+		}
 		users.id.push(userInfo.id);
 		users.name.push(userInfo.name);
 		users.pw.push(userInfo.pw);
-
+		fs.writeFile("./src/databases/users.json", JSON.stringify(users));
 		return { success: true };
 	}
 }
